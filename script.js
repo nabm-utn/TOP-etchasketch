5b33788d51684d47ed754cc8421a83037a44a491
nabm-utn <nabm.utn@gmail.com>
2021-07-26 01:03:58 -0300

    add base html layout and basic js functionality

---
+++ script.js
@@ -0,0 +1,16 @@
+const container = document.querySelector(".container");
+container.style.display = "grid";
+container.style.gridTemplateRows = "repeat(16, 1fr)";
+container.style.gridTemplateColumns = "repeat(16, 1fr)";
+for (let col = 1; col < 17; col ++) {
+    for (let row = 1; row < 17; row ++) {
+        let pointDiv = document.createElement("div");
+        pointDiv.style.gridColumnStart = col;
+        pointDiv.style.gridColumnEnd = col + 1;
+        pointDiv.style.gridRowStart = row;
+        pointDiv.style.gridRowEnd = row + 1;
+        pointDiv.textContent = row.toString() + col.toString();
+        pointDiv.classList.add("point-div")
+        container.appendChild(pointDiv);
+    }
+}
